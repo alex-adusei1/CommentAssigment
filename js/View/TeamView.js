770d3560ea55cb3567e2349be506a52c5bda6094
boatemaa <boatemaantim@gmail.com>
2017-06-08 13:29:50 +0000

    Update TeamView.js

--- js/View/TeamView.js
+++ js/View/TeamView.js
@@ -5,3 +5,38 @@
  */
 
 
+//this is the view class for the commentAssignment
+var Comment_View= function(){
+this.start= function(){
+Broadcaster.suscribe('Comment Added', this.appendComment);
+
+}
+
+this.appendComment= function(comment){
+	
+	console.log(addComment(comment));
+}
+
+function addComment(comment) {
+        var tmpl = document.getElementById("dispaly").innerHTML;
+        
+        var $tmpl = $(tmpl);
+       
+         $tmpl.find('.message').html(comment.getMessage());
+
+        $(list).append($tmpl);
+
+        
+    }
+
+   
+    this.render = function (comment) {
+       
+
+        for (var i = 0; i < comment.length; i++) {
+            addComment(comment[i]);
+        }
+    }
+
+    this.start();
+}
