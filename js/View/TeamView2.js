3d78d4a154ccfa31b9bdf254382cd0bd2eac6156
Alex Adusei <alex.a.adusei@gmail.com>
2017-06-08 15:19:07 +0000

    number added

--- js/View/TeamView2.js
+++ js/View/TeamView2.js
@@ -7,11 +7,12 @@ var TeamView2 = function(){
       var ul = document.createElement("ul"); 
       
       for(var i = 0; i < arr_content.length; i++){
-          var comment = arr_content[i];
+          var comment = (i+1) + ". "+arr_content[i] ;
           console.log(comment);
           
           li = document.createElement("li");
-          li.setAttribute("style", "list-style : none ");
+          li.setAttribute("style","border-bottom : 2px solid gray; list-style : none");
+          var hr = document.createElement("hr");
           
           li.innerHTML = comment;
           
